bc2228fc66ac04f08b2d1178c9cd57d8e5833636
larrylegend33 <andrewdbolton@fas.harvard.edu>
2023-11-06 16:23:43 -0500

    split into a dynamic and static snmc simulation

--- snmc_animation/spike.py
+++ snmc_animation/spike.py
@@ -2,9 +2,6 @@ import math
 import numpy as np
 import pygame as pg
 
-
-def electrode(x, y):
-    return 1 
     
 class Spike():
     
@@ -41,35 +38,166 @@ class Spike():
 # try to make this flexible so that you could eventually have the 
 # assemblies be generated on the fly. start with something very simple like a bernoulli draw.  
 
+class Resampler:
+    def __init__(self, particles, x, y):
+        self.particles = particles
+        self.x = x
+        self.y = y
+
+    def multiply_and_norm(self):
+        return 0
+
 class Particle:
-    def __init__(self, id, surface):
+    def __init__(self, samplescores, x, y):
+        self.samplescores = samplescores
+        self.x = x
+        self.y = y
+        
+# particle nests multiple SampleScores. Resampler takes particles.     
+
+class SampleScore:
+    def __init__(self, num_states, surface):
         self.length_of_simulation = 2000
         self.neurons_per_assembly = 2
-        self.num_states = 2 
+        self.num_states = num_states
         self.kq = 5
         self.kp = 10
         # can make this amenable to larger assembly sizes and more latents. 
         # hand code for now. 
-        self.assembly_indices = ["q0", "q1", "p0", "p1"]   
+        self.assembly_indices = ["q" + str(i) for i in range(self.num_states)] + [
+            "p" + str(i) for i in range(self.num_states)]
         self.assemblies = {i : [] for i in self.assembly_indices}
+        self.sim_lambdas = [np.random.uniform(0, .2) for i in range(num_states)]
         self.mux = {"q": [], 
                     "p": []}
         self.tik = {"q": [], 
                     "p": []}
         self.accum = {"q": []}
-        self.wta = {0: [],
-                    1: []}      
-        self.normalizer = []
-        self.resampler = []
+        self.wta = { i : [] for i in range(self.num_states) } 
         self.all_spikes = []
         self.surface = surface
+        
+    def detect_winner(self, time):
+        # turn this into a map over all q 
+        spike_at_t = list(map(lambda x: self.find_spikes_in_assemblies(time, "q" + str(x)),
+                              range(self.num_states)))
+        if sum(spike_at_t) > 0:
+            spiking_assemblies = np.nonzero(spike_at_t)[0]
+            if len(spiking_assemblies) == 1:
+                return spiking_assemblies[0]
+            else:
+                return np.random.randint(spiking_assemblies[0], spiking_assemblies[-1])
+        else:
+            return float("NaN")
+        
+    def find_spikes_in_assemblies(self, time, assembly):
+        spikes = sum([self.assemblies[assembly][i][time] for i in range(self.neurons_per_assembly)])
+        return spikes
+        
+    def populate_wta_and_scoring(self):
+        spike_height = 5
+        spacing = 2
+        cortex_x = 600
+        cortex_y = 300
+        # elements have to be arranged in y-order   
+        wtas = [self.wta[i] for i in range(self.num_states)]
+        assemblies = [self.assemblies[pq + str(pq_ind)][n_id] for pq in ["p", "q"] for n_id in range(
+                       self.neurons_per_assembly) for pq_ind in range(self.num_states)]
+        scoring = [self.mux["p"], self.mux["q"], self.tik["p"], self.tik["q"], self.accum["q"]]
+        elements = wtas + assemblies + scoring
+        sp_train_ylocs = range(cortex_y, cortex_y + ((spike_height + spacing) * len(elements)), spike_height+spacing)
+        for i, elem in enumerate(elements):
+            self.assign_spikes(elem, cortex_x, sp_train_ylocs[i], spike_height)        
+
+# maybe a good idea here to just return the elements, and have assign spikes be outside the class. 
+            
+    def assign_spikes(self, spikelist, x, y, spike_height):
+        for t in range(self.length_of_simulation):
+            if spikelist[t] != 0:
+                self.all_spikes.append(Spike(x, y, t, spike_height, self.surface))            
+        
+
+class SampleScore_RealTime(SampleScore):
+
+    def __init__(self, num_states, surface):
+        self.state = float("NaN")
+        SampleScore.__init__(self, num_states, surface)
+        
+    def poisson_spike(self):
+        for assembly_neuron in self.assemblies.keys():
+            rate = self.sim_lambdas[int(assembly_neuron[1:])]
+            self.assemblies[assembly_neuron].append(np.random.poisson(rate, 1)[0])
+
+    def run_snmc(self):
+        # have to implement counters too. 
+        p_tik = 0
+        q_tik = 0
+        for t in range(self.length_of_simulation):
+            self.poisson_spike()
+            if math.isnan(state):
+                state = self.detect_winner(t)            
+            if not math.isnan(state):
+                qmux_spikes = self.find_spikes_in_assemblies(t, "q" + str(state))
+                pmux_spikes = self.find_spikes_in_assemblies(t, "p" + str(state))
+# this is just to space the wta spikes a bit – it looks like one big bar.                 
+                if np.random.random() > .6:
+                    self.wta[state].append(1)
+                else:
+                    self.wta[state].append(0)
+                # when more than one state, just add a list here for all !state
+                for i in range(self.num_states):
+                    if i != state:
+                        self.wta[i].append(0)
+                total_p_assembly_spikes = sum(map(
+                    lambda x: self.find_spikes_in_assemblies(t, "p" + str(x)), range(self.num_states)))
+                total_q_assembly_spikes = sum(map(
+                    lambda x: self.find_spikes_in_assemblies(t, "q" + str(x)), range(self.num_states)))
+                p_tik += total_p_assembly_spikes
+                if p_tik <= self.kp:
+                    self.mux["p"].append(pmux_spikes)
+                    if p_tik == self.kp:
+                        self.tik["p"].append(1)
+                    else:
+                        self.tik["p"].append(0)
+                else: 
+                    self.mux["p"].append(0)  
+                    self.tik["p"].append(0)                     
+                q_tik += qmux_spikes
+                if q_tik <= self.kq:
+                    self.mux["q"].append(qmux_spikes)
+                    self.accum["q"].append(total_q_assembly_spikes)
+                    if q_tik == self.kq:
+                        self.tik["q"].append(1)
+                    else:
+                        self.tik["q"].append(0)                    
+                else: 
+                    self.mux["q"].append(0)
+                    self.accum["q"].append(0)
+                    self.tik["q"].append(0)
+                if p_tik > self.kp and q_tik > self.kq:
+                    p_tik = 0
+                    q_tik = 0
+                    # END SIMULATION HERE, store the state in self.state
+            else:
+                self.mux["q"].append(0)
+                self.mux["p"].append(0)
+                for i in range(self.num_states):
+                    self.wta[i].append(0)
+                self.tik["p"].append(0)
+                self.tik["q"].append(0)
+                self.accum["q"].append(0) 
+
+                
+class SampleScore_Static(SampleScore):
     
+    def __init__(self, num_states, surface):
+        SampleScore.__init__(self, num_states, surface)
+
     def populate_assemblies(self):        
-        sim_lambdas = [np.random.uniform(0, .2) for i in self.assembly_indices]
-       # assembly_params = [s for s in sim_params for _ in (0, self.neurons_per_assembly)]
-        for ai, λ  in zip(self.assembly_indices, sim_lambdas):
-            self.assemblies[ai] = [poisson_process(λ, self.length_of_simulation)
-                                for n in range(self.neurons_per_assembly)] 
+        for ai self.assembly_indices:
+            self.assemblies[ai] = [poisson_process(self.sim_lambdas[int(ai[1:])],
+                                                   self.length_of_simulation)
+                                   for n in range(self.neurons_per_assembly)] 
                     
     def run_snmc(self):
         # have to implement counters too. 
@@ -82,11 +210,19 @@ class Particle:
             if not math.isnan(state):
                 qmux_spikes = self.find_spikes_in_assemblies(t, "q" + str(state))
                 pmux_spikes = self.find_spikes_in_assemblies(t, "p" + str(state))
-                self.wta[state].append(1)
+# this is just to space the wta spikes a bit – it looks like one big bar.                 
+                if np.random.random() > .6:
+                    self.wta[state].append(1)
+                else:
+                    self.wta[state].append(0)
                 # when more than one state, just add a list here for all !state
-                self.wta[not state].append(0)
-                total_p_assembly_spikes = pmux_spikes + self.find_spikes_in_assemblies(t, "p" + str(int(not state)))
-                total_q_assembly_spikes = qmux_spikes + self.find_spikes_in_assemblies(t, "q" + str(int(not state)))
+                for i in range(self.num_states):
+                    if i != state:
+                        self.wta[i].append(0)
+                total_p_assembly_spikes = sum(map(
+                    lambda x: self.find_spikes_in_assemblies(t, "p" + str(x)), range(self.num_states)))
+                total_q_assembly_spikes = sum(map(
+                    lambda x: self.find_spikes_in_assemblies(t, "q" + str(x)), range(self.num_states)))
                 p_tik += total_p_assembly_spikes
                 if p_tik <= self.kp:
                     self.mux["p"].append(pmux_spikes)
@@ -117,50 +253,19 @@ class Particle:
             else:
                 self.mux["q"].append(0)
                 self.mux["p"].append(0)
-                self.wta[0].append(0)
-                self.wta[1].append(0)
+                for i in range(self.num_states):
+                    self.wta[i].append(0)
                 self.tik["p"].append(0)
                 self.tik["q"].append(0)
                 self.accum["q"].append(0)
      
-          
-    def detect_winner(self, time):
-        spike_in_0 = self.find_spikes_in_assemblies(time, "q0")
-        spike_in_1 = self.find_spikes_in_assemblies(time, "q1")
-        if spike_in_0 and not spike_in_1:
-            return 0 
-        elif spike_in_1 and not spike_in_0:
-            return 1
-        elif spike_in_1 and spike_in_0:
-            return bernoulli(.5) 
-        else:
-            return float("NaN")
-        
-    def find_spikes_in_assemblies(self, time, assembly):
-        spikes = sum([self.assemblies[assembly][i][time] for i in range(self.neurons_per_assembly)])
-        return spikes
-        
-    def populate_wta_and_scoring(self):
-    
-        spike_height = 5
-        spacing = 2
-        cortex_x = 600
-        cortex_y = 300
-        # elements have to be arranged in y-order   
-        wtas = [self.wta[0], self.wta[1]]
-        assemblies = [self.assemblies[pq + str(pq_ind)][n_id] for pq in ["p", "q"] for n_id in range(
-                       self.neurons_per_assembly) for pq_ind in range(self.num_states)]
-        scoring = [self.mux["p"], self.mux["q"], self.tik["p"], self.tik["q"], self.accum["q"]]
-        elements = wtas + assemblies + scoring 
-        sp_train_ylocs = range(cortex_y, cortex_y + ((spike_height + spacing) * len(elements)), spike_height+spacing)
-        for i, elem in enumerate(elements):
-            self.assign_spikes(elem, cortex_x, sp_train_ylocs[i], spike_height)        
-                    
-    def assign_spikes(self, spikelist, x, y, spike_height):
-        for t in range(self.length_of_simulation):
-            if spikelist[t] != 0:
-                self.all_spikes.append(Spike(x, y, t, spike_height, self.surface))            
-        
+    def start_snmc(self):
+        self.populate_assemblies()
+        self.run_snmc()
+        self.populate_wta_and_scoring()
+   
+
+                
 bernoulli = lambda p: np.random.binomial(1, p)
 
 def poisson_process(λ, num_timepoints):
--- snmc_animation/spike_anim.py
+++ snmc_animation/spike_anim.py
@@ -1,4 +1,4 @@
-from spike import Spike, Particle, electrode
+from spike import Spike, Particle, SampleScore, electrode
 import pygame as pg
 import numpy as np
 from scipy.stats import bernoulli
@@ -6,7 +6,7 @@ from scipy.stats import bernoulli
 # will have a frame count. if a spike with that init time is present in a particle, 
 # add it to the spike_queue. 
 
-def render_snmc_sim():
+def render_snmc():
      # traverse a permutation first.
     spike_queue = []
     num_particles = 1
@@ -23,12 +23,14 @@ def render_snmc_sim():
     brain = pg.image.load("MouseBrain.png")
 #    electrode_r = loadImage("Electrode.png")
 #    electrode_l = loadImage("Electrode_Reflected.png")
-    particles = [Particle(i, win) for i in range(num_particles)]
-    for p in particles:
-        p.populate_assemblies()
-        p.run_snmc()
-        p.populate_wta_and_scoring()
-        spike_queue.extend(p.all_spikes)
+
+# quick hack here is having x and y coord of activity be member variables of the SampleScore class. 
+    samplescores = [SampleScore(5, win) for i in range(num_particles)]
+    for s in samplescores:
+        s.populate_assemblies()
+        s.run_snmc()
+        s.populate_wta_and_scoring()
+        spike_queue.extend(s.all_spikes)
     win = pg.display.set_mode((scene_dim, scene_dim))
     running = True
     frame = 0
@@ -37,7 +39,7 @@ def render_snmc_sim():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-        pg.time.wait(100)
+        pg.time.wait(50)
         for sp in spike_queue:
              sp.move_in_time(frame)
              sp.display(frame)
@@ -46,3 +48,6 @@ def render_snmc_sim():
         pg.display.update()
         frame += 1
     pg.quit()
+
+# write a stripped down multiply and normalize here
+# log(P / Kp) + log( 1/Q   / Kq)
